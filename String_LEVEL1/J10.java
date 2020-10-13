a49bc862c2b55fc60d013e469f67ea7a7bfde6e1
AnupamJain01 <64133663+AnupamJain01@users.noreply.github.com>
2020-10-13 11:14:40 +0530

    Create J10.java

---
+++ String_LEVEL1/J10.java
@@ -0,0 +1,13 @@
+package WebT;
+
+import java.util.Scanner;
+
+public class J10 {
+	public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String s=sc.nextLine();
+        int i;
+        for(i=3;i<s.length();i++) 
+        System.out.print(s.charAt(i));
+	}
+}
