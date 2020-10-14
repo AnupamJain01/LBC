66bdd8034ecd67169383bdf2a3b3c9b9f716ef55
AnupamJain01 <64133663+AnupamJain01@users.noreply.github.com>
2020-10-14 14:52:13 +0530

    Create J14.java

---
+++ String_LEVEL1/J14.java
@@ -0,0 +1,13 @@
+package WebT;
+import java.util.*;
+public class J14 {
+
+	public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);              
+		 String s=sc.nextLine();
+		 s=s.replace("15 august","26 january");
+		 s=s.replace("independence","republic");
+		 System.out.println(s);
+	}
+
+}
