0f0cbabcc47682a2f2b78aac7d9fafa3a0fce745
AnupamJain01 <64133663+AnupamJain01@users.noreply.github.com>
2020-10-15 09:31:32 +0530

    Create J17.java

---
+++ String_LEVEL1/J17.java
@@ -0,0 +1,39 @@
+package WebT;
+
+import java.util.Scanner;
+
+public class J17 {
+
+public static void main(String[] args) {
+		
+    Scanner sc = new Scanner(System.in);
+    String s=sc.nextLine();
+    int i;
+    char c[]=new char[s.length()];
+    for(i=0;i<s.length();i++)
+    {
+      c[i]=s.charAt(i);
+    }
+
+
+
+    for(i=0;i<s.length();i++)
+    {
+      if(c[i]>=65 && c[i]<=90)
+        { 
+        int a=c[i];
+        c[i]=(char)(a+32);
+        }
+      else if(c[i]>=97 && c[i]<=122)
+        {
+        int a=c[i];
+        c[i]=(char)(a-32);
+        }
+   }
+   for(i=0;i<s.length();i++)
+   {
+       System.out.print(c[i]);
+   }
+}
+
+}
