3af050570a74e510554fb62718ef7674f42743fe
AnupamJain01 <64133663+AnupamJain01@users.noreply.github.com>
2020-10-15 09:36:53 +0530

    Create J20.java

---
+++ String_LEVEL1/J20.java
@@ -0,0 +1,17 @@
+package WebT;
+import java.util.*;
+public class J20 {
+
+	public static void main(String[] args) {
+		
+
+            Scanner sc = new Scanner(System.in);
+	    String s=sc.nextLine();
+	    String t[]=s.split(" ");
+	    for(String e:t) {
+                if(e.length()>5)
+	    	        System.out.println(e);
+	    }
+	}
+
+}
