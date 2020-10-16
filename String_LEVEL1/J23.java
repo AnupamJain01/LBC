77b9bf785ef58adc6a981d9afcc18eaaafa79944
AnupamJain01 <64133663+AnupamJain01@users.noreply.github.com>
2020-10-16 10:21:42 +0530

    Create J23.java

---
+++ String_LEVEL1/J23.java
@@ -0,0 +1,26 @@
+package WebT;
+import java.util.*;
+public class J23 {
+
+	public static void main(String[] args) {
+		
+           Scanner sc = new Scanner(System.in);
+           int i,a[]=new int[50];
+           String s[]=new String[500];
+           for(i=0;i<5;i++)
+           {
+              a[i]=sc.nextInt();
+              sc.nextLine();
+                    s[i]=sc.nextLine();
+           }
+           for(i=0;i<5;i++)
+           {
+              System.out.println(a[i]+"   "+s[i]);
+           }
+   
+		
+		
+		
+	}
+
+}
