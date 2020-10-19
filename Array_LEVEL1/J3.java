b23474c23a943080eb2be3eb6b8eb9a1b9efbf32
AnupamJain01 <64133663+AnupamJain01@users.noreply.github.com>
2020-10-19 14:24:02 +0530

    Create J3.java

---
+++ Array_LEVEL1/J3.java
@@ -0,0 +1,13 @@
+package arrayL1;
+import java.util.*;
+public class J3 {
+
+	public static void main(String[] args) {
+		
+         Scanner sc =new Scanner(System.in);
+         int m=sc.nextInt();
+         int a[]= {31,29,31,30,31,30,31,31,30,31,30,31};
+         System.out.println(a[m-1]);
+	}
+
+}
