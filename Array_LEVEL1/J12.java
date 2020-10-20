b8c70a9cd9efb0205e011d5d9dc48e851480c02f
AnupamJain01 <64133663+AnupamJain01@users.noreply.github.com>
2020-10-20 11:45:25 +0530

    Create J12.java

---
+++ Array_LEVEL1/J12.java
@@ -0,0 +1,26 @@
+package arrayL1;
+
+import java.util.Scanner;
+
+public class J12 {
+
+	public static void main(String[] args) {
+		
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+      	  a[i]=sc.nextInt();
+        int sum=0,count=0;
+        for(int i=0;i<n;i++)
+        {
+        	if(a[i]%2==0)
+        		{
+        		count++;
+        		sum+=a[i];
+        		}
+        }
+        System.out.println(sum/count);
+	}
+
+}
